2d156a3e8f3901266ebd516d59419cff877e186e
Zander Bolgar <lezed1@gmail.com>
2018-01-22 06:09:17 -0500

    Restarting, woo!

---
+++ install/pip-install.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+set -xeuo pipefail
+
+packages=(
+    pyserial
+    watchdog
+
+    cython
+    flask
+    gevent
+    matplotlib
+    paramiko
+    pyyaml
+    redis
+    requests
+    scipy
+    six
+    tabulate
+    termcolor
+    tornado
+
+    eventlet
+    posix_ipc
+
+    pygobject
+
+    nanomsg
+)
+
+packages2=(
+    posix_ipc
+    pygame
+)
+
+packages3=(
+    flask
+    flask-socketio
+    posix_ipc
+)
+
+pip2 install "${packages[@]}" "${packages2[@]}"
+pip3 install "${packages[@]}" "${packages3[@]}"
