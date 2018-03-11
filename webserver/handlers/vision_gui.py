89c4380e26465b643eed2c3e49d64343f5f78d94
Chesley Tan <chesleytan97@gmail.com>
2018-03-11 15:14:06 -0400

    Avoid unnecessary broadcast; Code cleanup

--- webserver/handlers/vision_gui.py
+++ webserver/handlers/vision_gui.py
@@ -1,10 +1,7 @@
-import sys
 import os
 import json
 import time
-import functools
 import collections
-import threading
 import traceback
 import base64
 import cv2
@@ -25,12 +22,13 @@ websocket_listeners = []
 
 VISION_GUI_PATH_PREFIX = '/vision-gui'
 
+
 def get_active_modules():
     prefix = "auv_visiond-module-"
     return [block[len(prefix):] for block in os.listdir('/dev/shm') if block.startswith(prefix)]
 
 
-def send_image(module_name, image_name, image):
+def send_image(module_name, image_name, image, receivers=websocket_listeners):
     try:
         if module_name not in module_frameworks:
             return
@@ -43,15 +41,15 @@ def send_image(module_name, image_name, image):
         _, jpeg = cv2.imencode('.jpg', image, (cv2.IMWRITE_JPEG_QUALITY, 60))
         jpeg_bytes = base64.encodestring(jpeg.tobytes()).decode('ascii')
         value_dict = {'image_name': image_name,
-                    'image': jpeg_bytes,
-                    'image_index': idx}
-        for websocket_listener in websocket_listeners:
+                      'image': jpeg_bytes,
+                      'image_index': idx}
+        for websocket_listener in receivers:
             websocket_listener.write_message(value_dict)
     except Exception as e:
         print(e)
 
 
-def send_option(module_name, option_name, value):
+def send_option(module_name, option_name, value, receivers=websocket_listeners):
     try:
         if module_name not in module_frameworks:
             return
@@ -63,7 +61,7 @@ def send_option(module_name, option_name, value):
         value_dict = {'option_name': option_name, 'option_index': idx}
         option.populate_value_dict(value_dict)
         #print("Sending option: " + str(value_dict))
-        for websocket_listener in websocket_listeners:
+        for websocket_listener in receivers:
             websocket_listener.write_message(value_dict)
     except Exception as e:
         print(e)
@@ -73,7 +71,6 @@ class VisionGuiSocketHandler(tornado.websocket.WebSocketHandler):
     def __init__(self, *args, **kwargs):
         super(VisionGuiSocketHandler, self).__init__(*args, **kwargs)
         self.module_name = None
-
 
     def open(self, module_name):
         print("Received connection to vision gui")
@@ -87,14 +84,12 @@ class VisionGuiSocketHandler(tornado.websocket.WebSocketHandler):
 
         websocket_listeners.append(self)
 
-        # TODO prob does not need to broadcast to everyone
         all_option_values = module_frameworks[module_name].get_option_values()
         for option_name, option_value in all_option_values.items():
-            send_option(module_name, option_name, option_value)
+            send_option(module_name, option_name, option_value, receivers=self)
         images = module_frameworks[module_name].get_images()
         for image_name, image in images.items():
-            send_image(module_name, image_name, image)
-
+            send_image(module_name, image_name, image, receivers=self)
 
     def on_message(self, message):
         data = json.loads(message)
@@ -110,7 +105,6 @@ class VisionGuiSocketHandler(tornado.websocket.WebSocketHandler):
         value_list = list(module_frameworks[module].get_option_values()[option_name][1])
         value_list[0] = new_value
         module_frameworks[module].write_option(option_name, value_list)
-
 
     def on_close(self):
         print("Connection to vision gui closed")
@@ -140,14 +134,12 @@ class VisionGuiModuleHandler(BaseHandler):
 
         return observe
 
-
     def get_option_observer(self, module_name):
         def observe(name, value):
             if module_listeners[module_name] == 0:
                 return
             send_option(module_name, name, value)
         return observe
-
 
     def initialize_module(self, module_name):
         if module_name in module_frameworks:
@@ -165,11 +157,10 @@ class VisionGuiModuleHandler(BaseHandler):
         accessor.register_option_observer(option_observer)
 
         accessor.register_cmf_deletion_callback(
-        lambda module_name=module_name: self.initialize_module(module_name)
+            lambda module_name=module_name: self.initialize_module(module_name)
         )
 
         module_frameworks[module_name] = accessor
-
 
     def get(self, module_name):
         if module_name not in get_active_modules():
@@ -192,14 +183,11 @@ class VisionGuiModuleHandler(BaseHandler):
         return self.render('vision_gui_module.html', path_prefix=VISION_GUI_PATH_PREFIX, module_name=module_name,
                            modules=get_active_modules())
 
-
-
     def write_error(self, status_code, **kwargs):
         if status_code == 404:
             self.finish("Could not find the requested module")
         elif status_code == 412:
-            self.finish("That module is not currently running" \
-                " i.e. It's controlled by a shm variable and that variable is 0")
+            self.finish("That module is not currently running"
+                        " i.e. It's controlled by a shm variable and that variable is 0")
         else:
             super(VisionGuiModuleHandler, self).write_error(status_code, **kwargs)
-
