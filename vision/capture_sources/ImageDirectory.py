44c356ffb3b72850a00b5ef0b4af0b024f197559
Alex Renda <alex@renda.org>
2018-04-18 17:03:12 -0400

    update imagedirectory for the 21st century :)

--- vision/capture_sources/ImageDirectory.py
+++ vision/capture_sources/ImageDirectory.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python3
+
+import argparse
 import os
 import time
 import itertools
+import sys
 
 import cv2
 import shm
@@ -12,22 +16,24 @@ class ImageDirectory(CaptureSource.CaptureSource):
         super().__init__(direction)
         self.directory = directory
 
-        files = [os.path.join(directory, f) for f in os.listdir(directory) if
-                 os.path.isfile(os.path.join(directory, f))]
+        files = []
+        for name in os.listdir(directory):
+            fname = os.path.join(directory, name)
+            im = cv2.imread(fname)
+            if im is not None:
+                files.append(fname)
+
         if loop:
-            self.files = itertools.cycle(files)
+            self.file_iter = itertools.cycle(files)
         else:
-            self.files = iter(files)
+            self.file_iter = iter(files)
 
         self.fps = float(fps)
         self.loop = loop
         self.last_time = time.time()
 
     def acquire_next_image(self):
-        try:
-            next_file = next(files)
-        except StopIteration:
-            return time.time(), None
+        next_file = next(self.file_iter)
         next_image = cv2.imread(next_file)
 
         time_to_sleep = 1 / self.fps - (time.time() - self.last_time)
@@ -38,3 +44,17 @@ class ImageDirectory(CaptureSource.CaptureSource):
         self.last_time = acq_time
 
         return next_image, acq_time
+
+def main():
+    parser = argparse.ArgumentParser(description='Process a directory of images as a video')
+    parser.add_argument('direction', help='direction to act as a camera')
+    parser.add_argument('directory', help='directory of images')
+    parser.add_argument('--fps', default=60.0, type=float, help='FPS to run the module at')
+
+    args = parser.parse_args()
+
+    cap = ImageDirectory(args.direction, args.directory, fps=args.fps)
+    cap.acquisition_loop()
+
+if __name__ == '__main__':
+    main()
