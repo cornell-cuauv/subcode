a41648dba21f57dc7acf115b4c6f39a0611b1cec
Zander Bolgar <lezed1@gmail.com>
2018-04-24 20:56:09 -0400

    This is a squashed commit from our repo migration!
    commit b3e81b926da8463cb3a987dcb077a02fb3fbb6d3
    Author: Kerri Diamond <kd333@cornell.edu>
    Date:   Tue Apr 24 20:46:10 2018 -0400
    
        Fixed angle calc error.
    
    commit 7671327be623031230a4c1388c58cab3bfbe9cf7
    Merge: d1e178102 9afb3f4fe
    Author: Kerri Diamond <kd333@cornell.edu>
    Date:   Tue Apr 24 19:43:38 2018 -0400
    
        Merge branch 'master' of ssh://bitbucket.cuauv.org:7999/sof/software into path
    
    commit d1e17810223acfbb857265963bfd58ede5e501e6
    Author: Kerri Diamond <kd333@cornell.edu>
    Date:   Tue Apr 24 19:43:13 2018 -0400
    
        Vision much improved.
    
    commit a8ec7f9fd99d18315a5e67a8410f3d9fed5917b7
    Author: Kerri Diamond <kd333@cornell.edu>
    Date:   Tue Apr 17 21:15:38 2018 -0400
    
        Line tracking working better
    
    commit 8fac4b262386bd40ccaa95584d41879d82aad6df
    Author: Kerri Diamond <kd333@cornell.edu>
    Date:   Sat Apr 14 18:17:50 2018 -0400
    
        Still not working, but better
    
    commit 3b5d7a061ee1a938a7dab4d309e575e030cc47b0
    Author: Kerri Diamond <kd333@cornell.edu>
    Date:   Wed Apr 11 16:58:42 2018 -0400
    
        Progress I hope
    
    commit 4d04a1ecf7faa937031de1dbca77714d670a66a1
    Merge: b9a4995ed 2dc246547
    Author: Kerri Diamond <kd333@cornell.edu>
    Date:   Tue Mar 27 19:41:02 2018 -0400
    
        Merge branch 'master' of ssh://bitbucket.cuauv.org:7999/sof/software into path
    
    commit b9a4995edaf245a0c7eeb66c6d20d08894baa2c3
    Author: Kerri Diamond <kd333@cornell.edu>
    Date:   Sun Mar 4 15:45:07 2018 -0500
    
        Small updates
    
    commit f69d18c414428308a2894a5ef7b924a9f0afa9cd
    Author: Kerri Diamond <kd333@cornell.edu>
    Date:   Tue Feb 27 20:53:29 2018 -0500
    
        Preliminary attempt at tracking.
    
    commit 039b56e69065e3d72dcdce923b237c74cf042b17
    Merge: 884768ab0 f0815e6aa
    Author: Kerri Diamond <kd333@cornell.edu>
    Date:   Tue Feb 13 19:10:33 2018 -0500
    
        Merge branch 'master' of ssh://bitbucket.cuauv.org:7999/sof/software into path
    
    commit 884768ab0865cdfe63222f58f1362a2dbcb9f268
    Author: Kerri Diamond <kd333@cornell.edu>
    Date:   Tue Feb 13 19:10:15 2018 -0500
    
        Updates to vision module
    
    commit ca3213febf6dee974eafdeac0bb87bd6bf7a9331
    Merge: d7014b602 007f58239
    Author: Kerri Diamond <kd333@cornell.edu>
    Date:   Tue Jan 30 21:38:34 2018 -0500
    
        Merge branch 'master' of ssh://bitbucket.cuauv.org:7999/sof/software into path
    
    commit d7014b6023886af0b66c36f8d016ee380e59f9ff
    Author: Kerri Diamond <kd333@cornell.edu>
    Date:   Tue Jan 30 21:37:53 2018 -0500
    
        Started work on vision module for path task

---
+++ mission/missions/path.py
@@ -0,0 +1,258 @@
+#!/usr/bin/env python3
+
+import math
+
+import shm
+
+from mission.framework.combinators import Sequential, Concurrent, Retry, Conditional
+from mission.framework.helpers import get_downward_camera_center, ConsistencyCheck
+from mission.framework.movement import Depth, Heading, Pitch, VelocityX, VelocityY, RelativeToCurrentHeading
+from mission.framework.position import PositionalControl
+from mission.framework.primitive import Zero, Log, FunctionTask, Fail
+from mission.framework.search import SearchFor, VelocityTSearch, SwaySearch, PitchSearch
+from mission.framework.targeting import DownwardTarget, PIDLoop, HeadingTarget
+from mission.framework.task import Task
+from mission.framework.timing import Timer, Timed
+from mission.framework.jank import TrackMovementY, RestorePosY
+
+PATH_FOLLOW_DEPTH = .6
+PATH_SEARCH_DEPTH = .6
+PATH_RIGHT_FIRST = True
+
+class center(Task):
+    def update_data(self):
+        self.path_results = shm.path_results_1.get()
+
+    def on_first_run(self):
+        self.update_data()
+
+        path_found = self.path_results.visible > 0
+
+        self.centered_checker = ConsistencyCheck(8, 10)
+
+        self.center = DownwardTarget(lambda self=self: (self.path_results.center_x, self.path_results.center_y),
+                                     target=(0,0),
+                                     deadband=(.05,.05), px=0.5, py=0.5, dx=0.02, dy=0.02,
+                                     valid=path_found)
+        self.logi("Beginning to center on the path")
+
+
+    def on_run(self):
+        self.update_data()
+        self.center()
+
+        if not check_seen():
+            self.finish(success=False)
+
+        if self.centered_checker.check(self.center.finished):
+            self.center.stop()
+            self.finish()
+
+class align(Task):
+    def update_data(self):
+        self.path_results = shm.path_results_1.get()
+
+    def on_first_run(self):
+        self.update_data()
+
+        self.align = Heading(lambda: self.path_results.angle -180 + shm.kalman.heading.get(), deadband=0.1)
+
+        self.alignment_checker = ConsistencyCheck(49, 50)
+
+        path_found = self.path_results.visible > 0
+
+        self.center = DownwardTarget(lambda self=self: (self.path_results.center_x, self.path_results.center_y),
+                                     target=(0,0),
+                                     deadband=(.05,.05), px=0.2, py=0.2, dx=0.02, dy=0.02,
+                                     valid=path_found)
+
+    def on_run(self):
+        self.update_data()
+
+        self.align()
+        print("kal")
+        print(shm.kalman.heading.get())
+        print(self.path_results.angle -180 + shm.kalman.heading.get())
+        # self.center()
+
+        if not check_seen():
+            self.finish(success=False)
+
+        if self.alignment_checker.check(self.align.finished):
+            self.finish()
+
+search_task_behind = lambda: SearchFor(VelocityTSearch(forward=2,stride = 3, rightFirst=PATH_RIGHT_FIRST, checkBehind=True),
+                                lambda: shm.path_results_1.visible.get() > 0,
+                                consistent_frames=(10, 10))
+
+search_task= lambda: SearchFor(VelocityTSearch(forward=2,stride = 3, rightFirst=PATH_RIGHT_FIRST),
+                                lambda: shm.path_results_1.visible.get() > 0,
+                                consistent_frames=(10, 10))
+
+pitch_search_task = lambda: SearchFor(PitchSearch(30),
+                                      lambda: shm.path_results_1.visible.get() > 0,
+                                      consistent_frames=(6, 6))
+
+path_test = lambda: Sequential(Depth(PATH_SEARCH_DEPTH),
+                               search_task(), center(), align(),
+                               Depth(PATH_FOLLOW_DEPTH))
+
+pitch_path_test = lambda: Sequential(Depth(PATH_SEARCH_DEPTH),
+                          pitch_search_task(), Zero(),
+                          Concurrent(center(), Pitch(0)), Zero(),
+                          center(), align(), Depth(PATH_FOLLOW_DEPTH))
+
+def check_seen():
+    visible = shm.path_results_1.visible.get()
+
+    #print(visible)
+    if visible > 0:
+        return True
+    else:
+        #print('Lost Path!')
+        return False
+
+class Timeout(Task):
+    def on_first_run(self, time, task, *args, **kwargs):
+        self.timer = Timer(time)
+
+    def on_run(self, time, task, *args, **kwargs):
+        task()
+        self.timer()
+        if task.finished:
+          self.finish()
+        elif self.timer.finished:
+          self.logw('Task timed out in {} seconds!'.format(time))
+          self.finish()
+
+def one_path(grp):
+    return Timeout(45, Sequential(
+        Log('Going to Search Depth'),
+        Depth(PATH_SEARCH_DEPTH),
+        Zero(),
+        Log('Sway searching for path with Behind'),
+        TrackMovementY(search_task_behind()),
+          Retry(lambda: Sequential(
+            Zero(),
+            Log('Sway searching for path; may have been lost'),
+            TrackMovementY(search_task(), shm.jank_pos.y.get()),
+            Log('Centering on path'),
+            Conditional(
+              center(),
+
+              on_fail=Fail(Sequential(
+                Log("Path lost, Attempting to Restore Y pos"),
+                Zero(),
+                TrackMovementY(RestorePosY(.3), shm.jank_pos.y.get()),
+              ))
+            ),
+
+            Depth(PATH_FOLLOW_DEPTH),
+            Log('Aligning with path'),
+            Concurrent(
+                align(),
+                center(),
+                finite=False
+            ),
+            ), float("inf")),
+
+        Zero(),
+        Log('Aligned, moving forward'),
+        Timed(VelocityX(.4),3),
+        Zero()
+        #Depth(PATH_FOLLOW_DEPTH)
+    ))
+
+def test_path(grp):
+    return Timeout(45, Sequential(
+        # Log('Going to Search Depth'),
+        # Depth(PATH_SEARCH_DEPTH),
+        # Zero(),
+        # Log('Sway searching for path with Behind'),
+        # TrackMovementY(search_task_behind()),
+          Retry(lambda: Sequential(
+            Zero(),
+            Log('Centering on path'),
+            center(),
+            Depth(PATH_FOLLOW_DEPTH),
+            Log('Aligning with path'),
+            Concurrent(
+                align(),
+                center(),
+                finite=False
+            ),
+            ), float("inf")),
+
+        Zero(),
+        # Log('Aligned, moving forward'),
+        # Timed(VelocityX(.4),3),
+        # Zero()
+        # #Depth(PATH_FOLLOW_DEPTH)
+    ))
+
+full = test_path(shm.desires)
+
+def second_path(grp):
+    return Sequential(
+        Log('Going to Search Depth'),
+        Depth(PATH_SEARCH_DEPTH),
+        Retry(lambda: Sequential(
+            Zero(),
+            Log('Sway searching for path'),
+            search_task(),
+            Log('Centering on path'),
+            center(),
+
+            Depth(PATH_FOLLOW_DEPTH),
+            Log('Aligning with path'),
+            Concurrent(
+                align(),
+                center(),
+                finite=False
+            ),
+            ), float("inf")),
+        Zero(),
+        Log('Aligned, moving forward'),
+        Timed(VelocityX(.4),3),
+        Zero()
+        #Depth(PATH_FOLLOW_DEPTH)
+    )
+
+#paths_mission = second_path(shm.desires)
+
+def pitch_path(grp):
+    return Sequential(
+             Depth(PATH_SEARCH_DEPTH),
+             pitch_search_task(),
+             Zero(),
+             center(),
+             Pitch(0),
+             center(),
+
+             Depth(PATH_FOLLOW_DEPTH),
+
+             Concurrent(
+                 #center(),
+                 align(),
+                 finite=False,
+             ),
+             PositionalControl(),
+             Zero(),
+    )
+
+pitch_path_mission = pitch_path(shm.desires)
+
+
+
+class OptimizablePath(Task):
+  def desiredModules(self):
+    return [shm.vision_modules.Paths]
+
+  def on_first_run(self, grp):
+    self.subtask = one_path(grp)
+    self.has_made_progress = False
+
+  def on_run(self, grp):
+    self.subtask()
+    if self.subtask.finished:
+      self.finish()
