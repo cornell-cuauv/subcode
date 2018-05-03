41b0fca7dd65002b516520722e21a3bab2c4397b
Zander Bolgar <asb322@cornell.edu>
2018-05-03 08:23:08 +0000

    Explicitly configure wireshark

--- install/setup-user.sh
+++ install/setup-user.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
-groupadd -g wireshark
 useradd --create-home --shell /bin/bash --groups sudo,wireshark,dialout software
 echo "software:software" | chpasswd
 
