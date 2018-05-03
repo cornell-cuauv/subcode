87940c3be8803b264842f84c1fc9accdd7ea1586
Zander Bolgar <asb322@cornell.edu>
2018-05-03 06:21:30 +0000

    Updates to fix hydrobroadcast

--- install/setup-user.sh
+++ install/setup-user.sh
@@ -2,6 +2,7 @@
 set -xeuo pipefail
 
 useradd --create-home --shell /bin/bash --groups sudo software
+usermod -a -G wireshark software
 echo "software:software" | chpasswd
 
 echo "%sudo   ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers
