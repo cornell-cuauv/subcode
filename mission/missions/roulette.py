b0aaee0a43e11d80d883f4225bfc614953d70536
Will Smith <wds68@cornell.edu>
2018-06-28 08:15:42 +0000

    roulette mission work

--- mission/missions/roulette.py
+++ mission/missions/roulette.py
@@ -13,7 +13,7 @@ from mission.framework.combinators import (
 )
 #from mission.framework.helpers import call_if_function
 from mission.framework.targeting import DownwardTarget
-from mission.framework.timing import Timeout
+from mission.framework.timing import Timer
 from mission.framework.movement import (
     RelativeToInitialDepth,
     RelativeToCurrentDepth,
@@ -122,13 +122,13 @@ from mission.framework.primitive import (
 
 # We have three dropper mechanisms
 PISTONS = {
-    'green': (shm.actuator_desires.trigger_01, shm.actuator_desires.trigger_11),
+    'green': (shm.actuator_desires.trigger_11, shm.actuator_desires.trigger_01),
     'red': None,
     'gold': None,
 }
 
 # Seconds
-PISTON_DELAY = 0.5
+PISTON_DELAY = 1
 
 class DropBall(Task):
     def __init__(self, target_piston):
@@ -154,17 +154,32 @@ class DropBall(Task):
 #5 These values are for Teagle
 # Perhaps we should instead do this by determining the size in the camera
 DEPTH_STANDARD = 1.0
-DEPTH_TARGET_ALIGN_BIN = 3.0
-DEPTH_TARGET_DROP = 3.6
+DEPTH_TARGET_ALIGN_BIN = 2.5
+DEPTH_TARGET_DROP = 2.5
 
-BIN_CENTER = shm.bins_vision
-GREEN_CENTER = shm.bins_green0
+def interpolate_list(a, b, steps):
+    return [a + (b - a) / steps * i for i in range(steps)]
+
+def tasks_from_params(task, params):
+    return [task(param) for param in params]
+
+def tasks_from_param(task, param, length):
+    return [task(param) for i in range(length)]
+
+def interleave(a, b):
+    # https://stackoverflow.com/a/7946825
+    return [val for pair in zip(a, b) for val in pair]
+
+DEPTH_STEPS = interpolate_list(DEPTH_STANDARD, DEPTH_TARGET_ALIGN_BIN, 4)
+
+BIN_CENTER = {'x': shm.bins_vision.center_x, 'y': shm.bins_vision.center_y}
+#GREEN_CENTER = {'x': shm.bins_green0.centroid_x, 'y': shm.bins_vision.centroid_y}
+GREEN_CENTER = BIN_CENTER
 
 negator = lambda fcn: -fcn()
 
-# X and Y are flipped
-align_roulette_center = lambda: DownwardTarget((BIN_CENTER.center_y.get, negator(BIN_CENTER.center_x.get)), target=(0, 0), px=0.2, py=0.2)
-align_green_center = lambda: DownwardTarget((GREEN_CENTER.centroid_y.get, negator(GREEN_CENTER.centroid_x.get)), target=(0, 0), px=0.2, py=0.2)
+align_roulette_center = lambda: DownwardTarget((BIN_CENTER['x'].get, BIN_CENTER['y'].get), target=(0, 0), px=0.2, py=0.4)
+align_green_center = lambda: DownwardTarget((GREEN_CENTER['x'].get, GREEN_CENTER['y'].get), target=(0, 0), px=0.2, py=0.2)
 
 # TODO
 Bin = namedtuple('Bin', ['shm'])
@@ -178,19 +193,22 @@ Full = Retry(
         align_roulette_center(),
         Log('Descending on roulette'),
         MasterConcurrent(
-            Depth(DEPTH_TARGET_ALIGN_BIN),
+            # Descend slowly, not all at once
+            Sequential(*interleave(tasks_from_params(Depth, DEPTH_STEPS), tasks_from_param(Timer, 1, len(DEPTH_STEPS)))),
             align_roulette_center(),
         ),
         Log('Aligning with green bin'),
         align_green_center(),
         Log('Descending on green bin'),
-        MasterConcurrent(
-            Depth(DEPTH_TARGET_DROP),
-            align_green_center()
-        ),
+        #MasterConcurrent(
+        #    Depth(DEPTH_TARGET_DROP),
+        #    align_green_center(),
+        #),
         Log('Dropping ball'),
         DropBall(PISTONS['green']),
         Log('Returning to normal depth'),
         Depth(DEPTH_STANDARD),
     )
 , attempts=5)
+
+Dropper = DropBall(PISTONS['green'])
