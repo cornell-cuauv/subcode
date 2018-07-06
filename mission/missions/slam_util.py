ef3b4ef6e57bab6788b71e3313966af66bb1863f
William Smith <wds68@cornell.edu>
2018-07-06 12:44:55 -0400

    add more helper functions and tasks, in theory this should just work

--- mission/missions/slam_util.py
+++ mission/missions/slam_util.py
@@ -4,6 +4,10 @@ import numpy as np
 import shm
 from conf.vehicle import cameras
 
+from slam.slam_client import SlamClient
+
+from mission.framework.targeting import ForwardTarget, DownwardTarget
+
 # Vision coords: camera_x, camera_y, dist
 # Sub coords: X, Y, Z
 # Slam coords: north, east, depth
@@ -13,15 +17,30 @@ CAMERA_DIMS = [
     (shm.camera.downward_width.get(), shm.camera.downward_height.get()),
 ]
 
-def vision_to_sub(x, y, dist):
-    theta = x # TODO calibrate
-    phi = y # TODO calibrate
+CAMERA_CENTERS = [(CAMERA_DIM[0] / 2, CAMERA_DIM[1] / 2) for CAMERA_DIM in CAMERA_DIMS]
+
+# TODO calibrate to obtain values for these
+CAMERA_SCALES = [
+    [1, 1], # X, Y forward
+    [1, 1], # X. Y downward
+]
+
+def vision_to_sub(x, y, dist, camera):
+    theta = x * CAMERA_SCALES[camera][0]
+    phi = y * CAMERA_SCALES[camera][1]
 
     return np.array(
         dist * np.cos(theta) * np.cos(phi),
         dist * np.sin(theta) * np.cos(phi),
         dist * np.cos(theta) * np.sin(phi),
     )
+
+# Returns just X and Y
+def sub_to_vision(sub_coords, camera):
+    theta = np.arctan2(sub_coords[1], sub_coords[0])
+    phi = np.arctan2(sub_coords[2], sub_coords[0])
+
+    return (theta / CAMERA_SCALES[camera][0], phi / CAMERA_SCALES[camera][1])
 
 make_rotate = lambda theta: np.array([
     [np.cos(theta), -np.sin(theta), 0],
@@ -56,3 +75,20 @@ def slam_to_sub(slam_coords):
     add_depth = make_add_depth(kalman.depth)
 
     return np.dot(np.dot(slam_coords - add_depth, flip_depth).T, rotate).T
+
+slam = SlamClient()
+
+def camera_num(camera):
+    return 1 if camera in ['downward', 'down'] else 0
+
+def observe(name, x, y, dist, camera=0):
+    return slam.observe_landmark(name, *sub_to_slam(vision_to_sub(x, y, dist, check_camera(camera))), uncertainty=1)
+
+def request(name, camera):
+    return sub_to_vision(slam_to_sub(np.array(slam.request_landmark(name)[0])), check_camera(amera))
+
+def ForwardTargetObject(name, deadband):
+    return ForwardTarget(request(name, 0), CAMERA_CENTERS[0], deadband)
+
+def DownwardTargetObject(name, deadband):
+    return DownwardTarget(request(name, 1), CAMERA_CENTERS[1], deadband)
