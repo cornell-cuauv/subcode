b376ba5e42a97c65e20dba53bbcb684e48cddd28
Zander Bolgar <asb322@cornell.edu>
2018-07-17 07:21:39 +0000

    Add merge mappings and update contianer

---
+++ install/temp-install.sh
@@ -0,0 +1,4 @@
+rm /usr/lib/aarch64-linux-gnu/tegra/libnvidia-ptxjitcompiler.so
+rm /usr/lib/aarch64-linux-gnu/tegra/libnvidia-ptxjitcompiler.so.1
+ln -s /usr/lib/aarch64-linux-gnu/tegra/libnvidia-ptxjitcompiler.so.28.2.1 /usr/lib/aarch64-linux-gnu/tegra/libnvidia-ptxjitcompiler.so
+ln -s /usr/lib/aarch64-linux-gnu/tegra/libnvidia-ptxjitcompiler.so.28.2.1 /usr/lib/aarch64-linux-gnu/tegra/libnvidia-ptxjitcompiler.so.1
