4b73e50537829067e7b76e8abab818865c651d64
Zander Bolgar <asb322@cornell.edu>
2018-07-31 02:25:32 +0000

    Trogdor start on boot

--- trogdor/trogdor_boot.sh
+++ trogdor/trogdor_boot.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+if [[ "$(uname -m)" != "aarch64" ]]; then
+    exit 0
+fi
+
 trogdor start
 
-sleep 5
+sleep 30
 
 # Don't start the mission automatically (for now) uncomment for competition
 # screen -d -m auv-mission-runner
