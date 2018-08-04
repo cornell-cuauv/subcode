03606051c6daad020a8359868c5505d1b7743133
Zander Bolgar <asb322@cornell.edu>
2018-08-04 19:48:50 +0000

    Better runner

--- mission/runner2.py
+++ mission/runner2.py
@@ -9,6 +9,7 @@ gh = lambda: shm.switches.hard_kill.get()
 
 args = sys.argv[1:]
 
+subprocess.run(["rmdir", "/home/software/cuauv/workspaces/repo/mission/.mission_lock"])
 p = subprocess.Popen(["/home/software/cuauv/workspaces/worktrees/master/link-stage/auv-mission-runner", *args])
 h = gh()
 t = time.time()
@@ -18,11 +19,14 @@ while True:
     new_h = gh()
     new_t = time.time()
     if not new_h:
+        r = False
         t = new_t
 
     if new_t - t > 10 and not r:
         p.kill()
-        p = subprocess.Popen(["/home/software/cuauv/workspaces/worktrees/master/link-stage/auv-start-modules"])
+        subprocess.run(["rmdir", "/home/software/cuauv/workspaces/repo/mission/.mission_lock"])
+        p = subprocess.Popen(["/home/software/cuauv/workspaces/worktrees/master/link-stage/auv-mission-runner", *args])
+        print("restarting")
         r = True
 
     time.sleep(1/60)
