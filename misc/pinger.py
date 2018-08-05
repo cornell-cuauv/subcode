de609235d683fda57ca8d7c44f5da81e4a3fd3d3
Will Smith <wds68@cornell.edu>
2018-08-05 17:04:59 +0000

    add pinger leds script (Zander's)

---
+++ misc/pinger.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import shm
+import time
+
+last_ping = 0
+t = time.time()
+
+while True:
+    new_ping = shm.hydrophones_results_track.tracked_ping_count.get()
+    new_time = time.time()
+
+    if new_ping != last_ping:
+        last_ping = new_ping
+        t = new_time
+    
+    if new_time - t > 5:
+        shm.leds.starboard_color_red.set(255)
+        shm.leds.starboard_color_green.set(0)
+        shm.leds.starboard_color_blue.set(0)
+        shm.leds.port_color_red.set(255)
+        shm.leds.port_color_green.set(0)
+        shm.leds.port_color_blue.set(0)
+    else:
+        shm.leds.starboard_color_red.set(0)
+        shm.leds.starboard_color_green.set(255)
+        shm.leds.starboard_color_blue.set(0)
+        shm.leds.port_color_red.set(0)
+        shm.leds.port_color_green.set(255)
+        shm.leds.port_color_blue.set(0)
+
