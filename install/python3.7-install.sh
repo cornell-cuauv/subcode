1218164dfb51278a2a79eee887fea3b68a9459cd
Will Smith <wds68@cornell.edu>
2019-04-19 01:05:57 +0000

    oops

--- install/python3.7-install.sh
+++ install/python3.7-install.sh
@@ -5,7 +5,7 @@ cd python3.7-install
 
 PYTHON_VERSION="3.7.3"
 wget "https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tgz"
-tar -xvzf "Python${PYTHON_VERSION}.tgz"
+tar -xvzf "Python-${PYTHON_VERSION}.tgz"
 
 ./configure
 make
