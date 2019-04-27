1ddb9d8efc9a354299f2d1ddec410230341e4f48
Will Smith <wds68@cornell.edu>
2019-04-27 17:39:47 +0000

    more dependencies for pip

--- install/python3.7-install.sh
+++ install/python3.7-install.sh
@@ -1,6 +1,7 @@
 # Install Python 3.7
 
 apt-get install -y zlib1g-dev libffi-dev
+apt-get install -y libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
 
 mkdir python3.7-install
 cd python3.7-install
