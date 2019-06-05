cd0c3ed472c0edc1d12db86335813e7492800668
Chesley Tan <ct353@cornell.edu>
2019-06-05 15:41:15 +0000

    Fix path for templates

--- conf/gui/server.py
+++ conf/gui/server.py
@@ -5,18 +5,18 @@ from pathlib import Path
 from flask import Flask, render_template
 
 
-DIR = os.getenv("CUAUV_SOFTWARE")
-VEHICLE = os.getenv("CUAUV_VEHICLE")
+DIR = os.getenv('CUAUV_SOFTWARE')
+VEHICLE = os.getenv('CUAUV_VEHICLE')
 
 if DIR is None:
-    sys.stderr.write("CUAUV_SOFTWARE must be set to the root of the software repository.\n")
+    sys.stderr.write('CUAUV_SOFTWARE must be set to the root of the software repository.\n')
     sys.exit(1)
 if VEHICLE is None:
-    sys.stderr.write("CUAUV_VEHICLE must be set.\n")
+    sys.stderr.write('CUAUV_VEHICLE must be set.\n')
     sys.exit(1)
 
 
-app = Flask(__name__)
+app = Flask(__name__, root_path=os.path.dirname(os.path.realpath(__file__)))
 
 
 @app.route('/')
@@ -26,7 +26,7 @@ def index():
 
 @app.route('/config', methods=['GET'])
 def get_config():
-    with open(str(Path(DIR) / "conf" / "{}.json".format(VEHICLE))) as f:
+    with open(str(Path(DIR) / 'conf' / '{}.json'.format(VEHICLE))) as f:
         return f.read()
 
 
