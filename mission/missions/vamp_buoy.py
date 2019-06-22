d0063b70799c3c8dffce441af53e27356ea26367
Attilus Leung <yl787@cornell.edu>
2019-06-22 15:38:17 -0400

    stake added to visualizer, buoys mission fixed

--- mission/missions/vamp_buoy.py
+++ mission/missions/vamp_buoy.py
@@ -23,6 +23,8 @@ from mission.missions.will_common import Consistent
 from mission.missions.poly import polygon
 from mission.framework.search import SearchFor, SwaySearch, VelocitySwaySearch, MoveX
 
+from mission.framework.timing import Timer
+
 import shm
 
 CAM_CENTER = (shm.vamp_buoy_results.camera_x.get(), shm.vamp_buoy_results.camera_y.get())
@@ -55,7 +57,7 @@ def call_buoy_size():
 def any_buoy_size():
     return getattr(shm.vamp_buoy_results, "%s_size"%"draugr").get()
 
-def align_h():
+def align_call_h():
     return getattr(shm.vamp_buoy_results, "%s_align_h"%CALL).get()    
 
 def align_any_h():
@@ -105,41 +107,52 @@ class PIDVelocity(Task):
         RelativeToCurrentVelocityY(0)()
     
 Point = lambda px=0.3, py=0.0003, p=0.01, d=0.0005, db=0: Concurrent(
-            HeadingTarget(point=any_buoy_center(), target=CAM_CENTER, px=px, py=py, dy=d, dx=d, deadband=(db,db)),
+            HeadingTarget(point=any_buoy_center, target=CAM_CENTER, px=px, py=py, dy=d, dx=d, deadband=(db,db)),
             While(lambda: Log("center: %d, %d, target: %d, %d"%(CAM_CENTER[0], CAM_CENTER[1], any_buoy_center()[0], any_buoy_center()[1])), True))
 
-AlignAnyNormal = lambda px=0.15, py=0.0003, p=0.02, d=0.0005, db=0: Concurrent(
-            HeadingTarget(point=any_buoy_center(), target=CAM_CENTER, px=px, py=py, dy=d, dx=d, deadband=(db,db)),
+close_to = lambda point1, point2, db=10: abs(point1[0]-point2[0]) < db and abs(point1[1]-point2[1]) < db
+aligned = lambda align, db=2: abs(align) < db
+
+AlignAnyNormal = lambda px=0.15, py=0.0003, p=0.02, d=0.0005, db=0: MasterConcurrent(
+            Consistent(lambda: close_to(any_buoy_center(), CAM_CENTER) and aligned(align_any_h()), 0.05, 0.1, False, True),
+            HeadingTarget(point=any_buoy_center, target=CAM_CENTER, px=px, py=py, dy=d, dx=d, deadband=(db,db)),
             PIDVelocity(align_any_h, p=p, d=d, db=db),
             While(lambda: Log("align_h: %d"%(align_any_h(),)), True)) #TODO: Make VelY help with centering buoy
 
-CenterAnyBuoy= lambda px=0.004, py=0.0003, d=0.005, db=0: Concurrent(
-        ForwardTarget(point=any_buoy_center(), target=CAM_CENTER, px=px, py=py, dx=d, dy=d, deadband=(db,db)), 
-        While(lambda: Log("center: %d, %d, target: %d, %d"%(CAM_CENTER[0], CAM_CENTER[1], any_buoy_center()[0], any_buoy_center()[1])), True)
+CenterAnyBuoy = lambda px=0.004, py=0.0003, d=0.005, db=0: MasterConcurrent(
+            Consistent(lambda: close_to(any_buoy_center(), CAM_CENTER), 0.05, 0.1, False, True),
+            ForwardTarget(point=any_buoy_center, target=CAM_CENTER, px=px, py=py, dx=d, dy=d, deadband=(db,db)), 
+            While(lambda: Log("center: %d, %d, target: %d, %d"%(CAM_CENTER[0], CAM_CENTER[1], any_buoy_center()[0], any_buoy_center()[1])), True)
 )
 
-CenterCalledBuoy= lambda px=0.004, py=0.0003, d=0.005, db=0: Concurrent(
-        ForwardTarget(point=call_buoy_center(), target=CAM_CENTER, px=px, py=py, dx=d, dy=d, deadband=(db,db)),
-        While(lambda: Log("center: %d, %d, target: %d, %d"%(CAM_CENTER[0](), CAM_CENTER[1](), any_buoy_center()[0](), any_buoy_center()[1]())), True)
+CenterCalledBuoy = lambda px=0.004, py=0.0003, d=0.005, db=0: MasterConcurrent(
+            Consistent(lambda: close_to(call_buoy_center(), CAM_CENTER), 0.05, 0.1, False, True),
+            ForwardTarget(point=call_buoy_center, target=CAM_CENTER, px=px, py=py, dx=d, dy=d, deadband=(db,db)),
+            While(lambda: Log("center: %d, %d, target: %d, %d"%(CAM_CENTER[0], CAM_CENTER[1], any_buoy_center()[0], any_buoy_center()[1])), True)
 )
 
-AlignCalledNormal = lambda px=0.15, py=0.0303, p=0.02, d=0.0005, db=0: Concurrent(
-            HeadingTarget(point=call_buoy_center(), target=CAM_CENTER, px=px, py=py, dy=d, dx=d, deadband=(db,db)),
-            PIDVelocity(align_h, p=p, d=d, db=db),
-            While(lambda: Log("align_h: %d"%align_h()), True)) #TODO: Make VelY help with centering buoy
+
+AlignCalledNormal = lambda px=0.15, py=0.0303, p=0.02, d=0.0005, db=0: MasterConcurrent(
+            Consistent(lambda: close_to(call_buoy_center(), CAM_CENTER) and aligned(align_call_h()), 0.05, 0.1, False, True),
+            HeadingTarget(point=call_buoy_center, target=CAM_CENTER, px=px, py=py, dy=d, dx=d, deadband=(db,db)),
+            PIDVelocity(align_call_h, p=p, d=d, db=db),
+            While(lambda: Log("align_h: %d"%align_call_h()), True)) #TODO: Make VelY help with centering buoy
 
 ApproachCalled = lambda: Sequential(
-            VelocityX(2),
-            MasterConcurrent(Retry(lambda: Consistent(lambda: call_buoy_size() > SIZE_THRESH, 0.05, 0.1, False, True), attempts=20), #ADD EITHER LOSE SIGHT OF BUOY
+            VelocityX(.2, db=10),
+            MasterConcurrent(Consistent(lambda: call_buoy_size() > SIZE_THRESH, 0.05, 0.1, False, True), #ADD EITHER LOSE SIGHT OF BUOY
                 CenterCalledBuoy(),
                 While(lambda: Log("size: %d"%any_buoy_size()),True)),
             Zero())
 
 Ram = lambda: Sequential(Concurrent(AlignCalledNormal(), MoveX(1)), Zero())
 
+RamV = lambda: Sequential(MasterConcurrent(Timer(3), AlignCalledNormal(), VelocityX(.3)), Zero())
+
+
 ApproachAny = lambda: Sequential(
-            VelocityX(.2),
-            MasterConcurrent(Retry(lambda: Consistent(lambda: any_buoy_size() > SIZE_THRESH, 0.05, 0.1, False, True), attempts=20), #ADD EITHER LOSE SIGHT OF BUOY
+            VelocityX(.2, db=10),
+            MasterConcurrent(Consistent(lambda: any_buoy_size() > SIZE_THRESH, 0.05, 0.1, False, True), #ADD EITHER LOSE SIGHT OF BUOY
                 CenterAnyBuoy(),
                 While(lambda: Log("size: %d"%any_buoy_size()),True)),
             Zero())
@@ -158,7 +171,7 @@ Full = lambda: Sequential(
         Log('Searching for face'),
         Conditional(FunctionTask(lambda: which_buoy_visible == b), on_fail=SearchAndApproach),
         Log('Face found, ramming'),
-        Ram(),
+        RamV(),
         Log('Vamp_Buoy Complete')
      )
 
