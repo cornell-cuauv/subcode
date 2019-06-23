322bba0a70f3557cba7d5482249a127beb094b49
Chesley Tan <ct353@cornell.edu>
2019-06-23 22:26:03 +0000

    Fix shm

--- install/shm_complete.zsh
+++ install/shm_complete.zsh
@@ -7,15 +7,25 @@ python -c 'import shm; print(" ".join(shm.__all__[1:]))' | read -A _shm_subcomma
 #echo ${subcommands[@]}
 function _auv-shm-cli {
     local state
+    #local integer NORMARG
+    #_arguments -n -S -- \
+    #    'group: :->group' \
+    #    'variable: :->var' \
+    #    'value: :->var_val' \
+
+    #if [ $NORMARG == -1 ]; then NORMARG=1; done
+    #echo $NORMARG
     _arguments \
-        '1: :->group' \
-        '2: :->var' \
-        '3: :->var_val'
+        "1: :->group" \
+        "2: :->var" \
+        "3: :->var_val"
 
     case $state in
         group) _describe 'auv-shm-cli' _shm_subcommands ;;
         var)
             local -a group_vars
+            #echo $words[0] $NORMARG
+            #echo $words[$((NORMARG+1))]
             group_vars=("${(@f)$(grep _fields\.append $CUAUV_SOFTWARE/shm/${words[2]}.py | cut -d\" -f2)}")
             _describe 'auv-shm-cli' group_vars
             ;;
@@ -23,3 +33,8 @@ function _auv-shm-cli {
     esac
 }
 compdef _auv-shm-cli auv-shm-cli
+#        '-w' \
+#        '-r' \
+#        '--groups' \
+#        '-a[show all]' \
+
