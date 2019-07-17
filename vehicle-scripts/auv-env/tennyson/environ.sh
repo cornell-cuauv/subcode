ff4d1389b90429007c31bf743e06c00a274ed629
Tennyson T Bardwell <ttb33@cornell.edu>
2019-07-16 22:15:55 -0400

    --wip-- [skip ci]

--- vehicle-scripts/auv-env/tennyson/environ.sh
+++ vehicle-scripts/auv-env/tennyson/environ.sh
@@ -9,7 +9,9 @@ export NAME='Tennyson T Bardwell'
 
 export GIT_AUTHOR_NAME=$NAME
 export GIT_AUTHOR_EMAIL=$EMAIL
-export EMOJI=:chris2:
+export GIT_COMMITTER_NAME=$NAME
+export GIT_COMMITTER_EMAIL=$EMAIL
+
 export EDITOR="emacsclient -nw -c"
 export VISUAL=$EDITOR
 alias emc=$VISUAL
