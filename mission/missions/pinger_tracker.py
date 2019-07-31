29166e2e6e09cb842259e0e93a3b18f7ae07720a
Attilus Leung <yl787@cornell.edu>
2019-07-31 00:18:46 -0700

    pinger mission updated to terminate

--- mission/missions/pinger_tracker.py
+++ mission/missions/pinger_tracker.py
@@ -3,7 +3,7 @@ import shm
 
 import math
 
-from mission.constants.config import HYDROPHONES_PINGER_DEPTH
+# from mission.constants.config import HYDROPHONES_PINGER_DEPTH
 from mission.constants.region import PINGER_FREQUENCY, TRACK_MAG_THRESH, TRACK_COOLDOWN_SAMPLES
 
 from mission.framework.task import Task
@@ -66,3 +66,51 @@ track = Sequential(
         While(lambda: VelocityX(calc_speed), lambda: True),
     )
 )
+
+class TrackPinger(Task):
+
+    def on_first_run(self):
+        self.last_shmval = None
+        self.last_target_heading = None
+        self.last_target_elevation = 0
+        self.checker = ConsistencyCheck(count=5, total=7)
+
+    def update(self):
+        self.shmval = shm.hydrophones_results_track.tracked_ping_heading.get()
+        print('last: ' + str(self.last_shmval) + ', new: ' + str(self.shmval))
+        if self.last_shmval is None or self.shmval != self.last_shmval:
+            self.last_shmval = self.shmval
+            self.last_target_heading = self.shmval
+            self.last_target_elevation = shm.hydrophones_results_track.tracked_ping_elevation.get()
+            print('heading: ' + str(self.last_target_heading))
+            print('elevation: ' + str(self.last_target_elevation))
+            return self.checker.check(self.angle_diff(self.get_target_heading(), shm.kalman.heading.get()) > 90)
+        return False
+
+    def get_target_heading(self):
+        return self.last_target_heading
+
+    def get_target_elevation(self):
+        return self.last_target_elevation
+
+    def angle_diff(self, a, b):
+        return math.degrees(abs(math.atan2(math.sin(math.radians(a) - math.radians(b)),
+                                           math.cos(math.radians(a) - math.radians(b)))))
+
+    def calc_speed(self):
+        diff = self.angle_diff(self.get_target_heading(), shm.kalman.heading.get())
+        print("diff: " + str(diff))
+        if diff < 45:
+            print(min((45 - diff) / 20, 0.6))
+            return min((45 - diff) / 20, 0.6)
+        else:
+            print("0")
+            return 0
+
+    def on_run(self):
+        if self.update():
+            VelocityX(0)()
+            self.finish()
+            return
+        Heading(self.get_target_heading())()
+        VelocityX(self.calc_speed())()
