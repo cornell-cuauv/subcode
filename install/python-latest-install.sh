e684b364db0f81fd7427d49dd01ef7b9fe9781f3
Will Smith <wds68@cornell.edu>
2019-10-29 03:34:19 -0400

    we don't need python 3.5

--- install/python-latest-install.sh
+++ install/python-latest-install.sh
@@ -1,29 +1,27 @@
 # Install latest version of Python (currently 3.8.0)
 
-# apt-get install -y zlib1g-dev libffi-dev
-# apt-get install -y libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+apt-get install -y zlib1g-dev libffi-dev
+apt-get install -y libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
 
-# mkdir python-latest-install
-# cd python-latest-install
+mkdir python-latest-install
+cd python-latest-install
 
-# PYTHON_VERSION="3.8.0"
-# wget "https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tgz"
-# tar -xvzf "Python-${PYTHON_VERSION}.tgz"
+PYTHON_VERSION="3.8.0"
+wget "https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tgz"
+tar -xvzf "Python-${PYTHON_VERSION}.tgz"
 
-# cd "Python-${PYTHON_VERSION}"
+cd "Python-${PYTHON_VERSION}"
 
-# ./configure
-# make
-# make altinstall
+./configure
+make
+make install
 
-# rm -rf /python-latest-install
+rm -rf /python-latest-install
 
-add-apt-repository ppa:deadsnakes/ppa
-apt-get update
+# add-apt-repository ppa:deadsnakes/ppa
+# apt-get update
 
-apt-get install -y python3.8 python3.8-dev python3.8-distutils python3-pip
-python3 -m pip install pip
-python3 -m ensurepip
+# apt-get install -y python3.8 python3.8-dev python3.8-distutils
 
 pip3 install --upgrade pip
 
