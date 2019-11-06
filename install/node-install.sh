f4773aeda0060f689f6057e8eb96b98469b90d24
Will Smith <wds68@cornell.edu>
2019-11-06 00:54:02 +0000

    maybe we can nuke python2

--- install/node-install.sh
+++ install/node-install.sh
@@ -1,6 +1,6 @@
 curl -sL https://deb.nodesource.com/setup_13.x | bash -
 
-apt-get install -y nodejs
+apt-get install -y --no-install-recommends nodejs
 
 npm install -g webpack webpack-cli
 npm install -g npm-check-updates
