de1e282cf6cdaafe0c1044445172b0b5996fd01d
Oliver <ojm23@cornell.edu>
2020-11-12 01:29:57 +0000

    Trying to get docker image to work

--- install/misc-install.sh
+++ install/misc-install.sh
@@ -52,6 +52,7 @@ sudo -u software mkdir -p /home/software/.config
 sudo -u software ln -s /home/software/cuauv/software/install/nvim /home/software/.config/
 
 # **************** mypy ****************
+echo "Oliver you idioittttt"
 sudo -u software ln -s /home/software/cuauv/software/install/mypy /home/software/.config/
 
 # **************** sloth ****************
