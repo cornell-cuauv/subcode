82898663a7428b65422b4b9dce8a05b4fdf2f163
John Macdonald <jm2424@cornell.edu>
2021-07-25 00:05:33 +0000

    added hard reset script

---
+++ misc/hard_reset.sh
@@ -0,0 +1,15 @@
+
+BRANCH="$(git symbolic-ref HEAD | cut -f 3 -d /)"
+if [ "$BRANCH" = "master" ]; then
+    git fetch origin master
+    git checkout --force -B master origin/master
+    git reset --hard
+    git clean -fdx
+    git submodule update --init --recursive --force
+    git submodule foreach git fetch
+    git submodule foreach git checkout --force -B master origin/master
+    git submodule foreach git reset --hard
+    git submodule foreach git clean -fdx
+else
+    echo "Error: auv-hard-reset can only be used on the master branch."
+fi
