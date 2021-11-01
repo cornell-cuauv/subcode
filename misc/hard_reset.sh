a5b60b737d5d81d36e3e7b243a03f93cc6b5a730
aaron <apf75@cornell.edu>
2021-11-01 19:22:57 +0000

    Revert "wrote highly buggy blocking mission framework"
    This reverts commit c9728110c123c5179c06fb78beb74943c0506307.

--- misc/hard_reset.sh
+++ misc/hard_reset.sh
@@ -5,14 +5,6 @@ if [ "$BRANCH" = "master" ]; then
     git checkout --force -B master origin/master
     git reset --hard
     git clean -fdx
-<<<<<<< HEAD
-=======
-    git submodule update --init --recursive --force
-    git submodule foreach git fetch
-    git submodule foreach git checkout --force -B master origin/master
-    git submodule foreach git reset --hard
-    git submodule foreach git clean -fdx
->>>>>>> added hard reset script
 else
     echo "Error: auv-hard-reset can only be used on the master branch."
 fi
--- mission/bframework/task.py
+++
@@ -1,7 +0,0 @@
-class Task:
-    def __init__(self, *args, **kwargs):
-        self._args = args
-        self._kwargs = kwargs
-
-    def __call__(self):
-        self.run(*self._args, **self._kwargs)
