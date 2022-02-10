87133b3a57a2fb5447eda448d4abdff566fa17ff
aaron <apf75@cornell.edu>
2022-02-10 01:14:27 +0000

    cleaned up for code review

--- flamingo/runner.py
+++ flamingo/runner.py
@@ -8,35 +8,12 @@ import signal
 
 import shm
 from mission.framework.primitive import Zero
-from flamingo.framework import State, Condition
-
-parser = argparse.ArgumentParser()
-parser.add_argument('filename')
-parser.add_argument('--max-actions', type=int, default=float("inf"))
-parser.add_argument('--max-time', type=int, default=float("inf"))
-args = parser.parse_args()
+from flamingo.framework import State, Comparison
 
 def error(message):
     print('\033[1;31;40m' + str(message) + '\033[0m')
 
-# Import the mission file.
-try:
-    mission = importlib.import_module("missions." + args.filename)
-except Exception as e:
-    error("Something went wrong when importing " + args.filename + ":")
-    error(e)
-    sys.exit()
-
-# Check that all actions specify time.
-if args.max_time != float("inf") and any([action.time == None for action in mission.actions]):
-    error("Not all actions specify time, so --max-time will be ignored.")
-    args.max_time = float("inf")
-
-# Clean up on interrupt.
-vision_modules_state = shm.vision_modules.get()
-settings_control_state = shm.settings_control.get()
-navigation_settings_state = shm.navigation_settings.get()
-
+# Reset settings after termination.
 def cleanup(*args):
     Zero()()
     shm.vision_modules.set(vision_modules_state)
@@ -44,14 +21,12 @@ def cleanup(*args):
     shm.navigation_settings.set(navigation_settings_state)
     sys.exit(0)
 
-signal.signal(signal.SIGINT, cleanup)
-
 # Evaluate all the relevent SHM variables.
-def find_starting_state():
+def find_starting_state(mission):
     state = State()
     for action in mission.actions:
         for condition in action.preconds + action.invariants + action.postconds:
-            if isinstance(condition, Condition):
+            if isinstance(condition, Comparison):
                 for var, _ in condition.assumed_values().items():
                     state.shm_values[var] = var.get()
     return state
@@ -61,7 +36,7 @@ class SearchNode:
         self.state = state
         self.plan = plan
 
-    def score(self):
+    def score(self, mission):
         total = 0
         for goal, value in mission.goals.items():
             if goal in self.state.flags:
@@ -76,19 +51,19 @@ class SearchNode:
         return total
 
 # Find a list of actions to achieve as many goals as possible from a starting state.
-def solve(starting_state):
+def solve(starting_state, mission, max_actions, max_time):
     visited_states = set()
     queue = [SearchNode(starting_state, [])]
     best_plan = None
     best_plan_score = 0
     while len(queue) > 0:
         node = queue.pop(0)
-        if node.state in visited_states or len(node.plan) > args.max_actions or node.time() > args.max_time:
+        if node.state in visited_states or len(node.plan) > max_actions or node.time() > max_time:
             continue
         visited_states.add(node.state)
-        if node.score() > best_plan_score:
+        if node.score(mission) > best_plan_score:
             best_plan = node.plan
-            best_plan_score = node.score()
+            best_plan_score = node.score(mission)
         for action in mission.actions:
             if action.dependencies_functioning():
                 if node.state.satisfies_conditions(action.preconds) and not node.state.satisfies_conditions(action.postconds):
@@ -102,9 +77,9 @@ def solve(starting_state):
     sys.exit(0)
 
 # Find a plan and execute it, one action at a time.
-def find_and_execute_plan():
-    starting_state = find_starting_state()
-    plan = solve(starting_state)
+def find_and_execute_plan(mission, max_actions, max_time):
+    starting_state = find_starting_state(mission)
+    plan = solve(starting_state, mission, max_actions, max_time)
     print("Plan identified: " + ", ".join([action.name for action in plan]))
     for action in plan:
         print("Executing action: " + action.name)
@@ -116,6 +91,37 @@ def find_and_execute_plan():
             return False
     return True
 
-while not find_and_execute_plan():
-    time.sleep(0.1)
-cleanup()
+def main(filename, max_actions, max_time):
+    # Import the mission file.
+    try:
+        mission = importlib.import_module("missions." + filename)
+    except Exception as e:
+        error("Something went wrong when importing " + filename + ":")
+        error(e)
+        sys.exit()
+
+    # Check that all actions specify time.
+    if max_time != float("inf") and any([action.time == None for action in mission.actions]):
+        error("Not all actions specify time, so --max-time will be ignored.")
+        max_time = float("inf")
+
+    # Clean up on interrupt.
+    global vision_modules_state, settings_control_state, navigation_settings_state
+    vision_modules_state = shm.vision_modules.get()
+    settings_control_state = shm.settings_control.get()
+    navigation_settings_state = shm.navigation_settings.get()
+    
+    signal.signal(signal.SIGINT, cleanup)
+    
+    while not find_and_execute_plan(mission, max_actions, max_time):
+        time.sleep(0.1)
+    cleanup()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename')
+    parser.add_argument('--max-actions', type=int, default=float("inf"))
+    parser.add_argument('--max-time', type=int, default=float("inf"))
+    args = parser.parse_args()
+    
+    main(args.filename, args.max_actions, args.max_time)
